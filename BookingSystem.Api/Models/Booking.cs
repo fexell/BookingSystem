d920d939658ed7e9ac414a205d7708e421f659e8
ChloeNkm <nakamanyachloe4@gmail.com>
2026-03-18 21:45:13 +0100

    Add Models: User, Resource, Booking

---
+++ BookingSystem.Api/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace BookingSystem.Api.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Status { get; set; } = "Active";
+
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public int ResourceId { get; set; }
+        public Resource Resource { get; set; } = null!;
+
+    }
+}
