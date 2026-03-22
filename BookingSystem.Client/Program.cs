e83e4892570229d16782650a9b77ce0d21c234ec
Omar Balach <Balachomar@gmail.com>
2026-03-22 22:20:36 +0100

    "Klar med smart meny, AuthorizeView och inloggnings-skal"

--- BookingSystem.Client/Program.cs
+++ BookingSystem.Client/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using BookingSystem.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BookingSystem.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
---
+++ BookingSystem.Client/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookingSystem.Client.Services
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+
+            var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
